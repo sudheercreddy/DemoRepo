ad0b9106f64d3169174f0c5166aab99cbc20cbd7
sudheer <sudheerqa.selenium@gmail.com>
2019-10-30 00:37:21 -0500

    Adding all files to branch

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -1,7 +1,9 @@
 package com.qa.pages;
 
 public class SearchPage {
-
+public void fff(){
+	System.out.println("skdjfhksdjfh");
+}
 	public static void main(String[] args) {
 		System.out.println("Search page");
 
