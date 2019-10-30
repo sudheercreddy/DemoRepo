588c48a774c32e63f9fc6705a2b73f538dce33bb
sudheer <sudheerqa.selenium@gmail.com>
2019-10-29 22:31:43 -0500

    first git commit to selenium code

---
+++ src/main/java/com/qa/utils/TestUtil.java
@@ -0,0 +1,10 @@
+package com.qa.utils;
+
+public class TestUtil {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there ");
+
+	}
+
+}
---
+++ src/test/java/com/qa/tests/TestMethods.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+public class TestMethods {
+
+	public static void main(String[] args) {
+		System.out.println("This is test methods");
+
+	}
+
+}
