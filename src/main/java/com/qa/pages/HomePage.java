a4686e65ae0e90c40ba22b1cd3b0f1ee1716b8f8
sudheer <sudheerqa.selenium@gmail.com>
2019-10-29 23:00:55 -0500

    10/29/2019

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,7 +1,9 @@
 package com.qa.pages;
 
 public class HomePage {
-
+	public void bbb(){
+		System.out.println("Helleo ABCD");
+	}
 	public static void main(String[] args) {
 		System.out.println("this is home page");
 
--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package com.qa.pages;
 
 public class LoginPage {
-
+public void aaa(){
+	System.out.println("Helleo ABCD");
+}
 	public static void main(String[] args) {
 		System.out.println("This is LoginPage");
 
